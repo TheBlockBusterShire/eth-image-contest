551a1f66c0c33e1cecd2a93641ac0c7e0f6f545b
Kamil Talanda <kamil.talanda@gmail.com>
2021-11-28 07:42:35 +1100

    Combine contracts reduce deployment cost

---
+++ contracts/ImageVoterContract.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ImageVoterContract {
+    // ImageStorage -------------------------------------
+    struct ImageMetadata {
+        bytes32 id;
+        string imagehash;
+        string title;
+        uint256 createdAt;
+    }
+
+    address owner;
+
+    bytes32[] internal imageIds;
+    mapping(bytes32 => ImageMetadata) public images;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setImage(
+        bytes32 id,
+        string memory imagehash,
+        string memory title
+    ) public {
+        ImageMetadata memory imageMetadata = ImageMetadata(
+            id,
+            imagehash,
+            title,
+            block.timestamp
+        );
+
+        imageIds.push(id);
+        images[id] = imageMetadata;
+    }
+
+    function getImages() public view returns(ImageMetadata [] memory) {
+        ImageMetadata[] memory result = new ImageMetadata[](imageIds.length);
+        for (uint p = 0; p < imageIds.length; p++) {
+            bytes32 id = imageIds[p];
+            result[p] = images[id];
+        }
+        return result;
+    }
+
+    function burn(bytes32 id) public {
+        require(
+            msg.sender == owner,
+            "You are not allowed to remove image, only owner of smart contract can remove images."
+        );
+
+        for (uint p = 0; p < imageIds.length; p++) {
+            if (imageIds[p] == id) {
+                imageIds[p] = imageIds[imageIds.length-1];
+                imageIds.pop();
+            }
+        }
+    }
+    // --------------------------------------------------
+
+    // VoteStorage --------------------------------------
+    struct Vote {
+        address voterAddress;
+        bytes32 imageId;
+    }
+
+    address[] internal voters;
+    mapping(address => bytes32) votes;
+
+    function setVote(bytes32 imageId) public {
+        if (!userHasVoted()) voters.push(msg.sender);
+        votes[msg.sender] = imageId;
+    }
+
+    function getVotes() public view returns(Vote[] memory) {
+        Vote[] memory result = new Vote[](voters.length);
+        for (uint p = 0; p < voters.length; p++) {
+            address voterAddress = voters[p];
+            result[p] = Vote(voterAddress, votes[voterAddress]);
+        }
+        return result;
+    }
+
+    function burnVote() public {
+        for (uint p = 0; p < voters.length; p++) {
+            if (voters[p] == msg.sender) {
+                voters[p] = voters[voters.length-1];
+                voters.pop();
+            }
+        }
+    }
+
+    function userHasVoted() internal view returns(bool) {
+        for (uint p = 0; p < voters.length; p++) {
+            if (voters[p] == msg.sender) return true;
+        }
+        return false;
+    }
+    // --------------------------------------------------
+}
